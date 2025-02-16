4548faab9c0e0674672231fa0aaff4c0edcd4b2a
tiangenglu <58899759+tiangenglu@users.noreply.github.com>
2025-02-15 23:13:09 -0500

    summary results table cleaned and output

--- 2025-02-12_clean_lgl_txt.py
+++ 2025-02-12_clean_lgl_txt.py
@@ -51,18 +51,61 @@ loop_end_locations = np.where(l_start_array == all_precincts[-1])[0]
 print(f'This is the output without indexing[0]: {np.where(l_start_array == all_precincts[-1])}')
 print(f'This is the (correct) output when indexing[0] from np.where: {loop_end_locations}')
 
-# summary
-summary_raw = pd.DataFrame(line_by_line[:loop_end_locations[0]][1:])
 
 # concatenated column names, needs work
 colnames_raw = line_by_line[:loop_end_locations[0]][0]
 print(f'raw column names:\n {colnames_raw}')
-colnames_raw_list= colnames_raw.split('  ')
+colnames_raw_list= colnames_raw.split('  ') # ['Precinct', '', '', '', '', '', '', '', '', '', '', 'Registered Voters', '', '', '', '', '', ' Ballots Cast', '', '', '', '', '', '', '', '', 'Voter Turnout', '', '', '', '', '', '', '', ' \n']
 print(f'raw column names split by two spaces:\n {colnames_raw_list}')
 print([len(col) for col in colnames_raw_list])
 # get the finalized snake_case column names, strip(' ') to remove leading spaces and retain the non-empty and non-line-break characters
 colnames_list=[col.strip(' ').replace(' ','_').lower() for col in colnames_raw_list if len(col) > 2 and not col.endswith('\n')]
 print(f'After a few string manipulation steps, get the final column names: \n{colnames_list}')
+
+split_line_by_line = [None] * (loop_end_locations[0]+1) # zero indexing
+for i,line in enumerate(line_by_line[:loop_end_locations[0] + 1]):
+    split_line_by_line[i] = line_by_line[i].replace('\n','').replace('%','').split('  ')
+print('Print the first and last row of the data before cleaning:')    
+print(split_line_by_line[0])
+print(split_line_by_line[-1])
+
+clean_line_by_line = []
+# here, the range is 1, because 0 (the very first row) should be the header
+for i in range(1,loop_end_locations[0] + 1):
+    if split_line_by_line[i] is not None:
+        clean_line_by_line.append([l.strip() for l in split_line_by_line[i] if l is not None and len(l) > 0])
+print('Print the data in list form after removing the blank elements:')        
+print(clean_line_by_line[:5])
+print(clean_line_by_line[-5:])
+
+summary_results = pd.DataFrame(clean_line_by_line, columns=colnames_list)
+print('Create dataframe from the list, and then print the first and last several rows:')
+print(summary_results.head())
+print(summary_results.tail())
+print('Print the dataframe info:')
+print(summary_results.info())
+"""
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 161 entries, 0 to 160
+Data columns (total 4 columns):
+ #   Column             Non-Null Count  Dtype 
+---  ------             --------------  ----- 
+ 0   precinct           161 non-null    object
+ 1   registered_voters  161 non-null    object
+ 2   ballots_cast       161 non-null    object
+ 3   voter_turnout      161 non-null    object
+dtypes: object(4)
+memory usage: 5.2+ KB
+"""
+
+data_type_summary_results = dict(zip(colnames_list,['str','int','int','float']))
+print(data_type_summary_results)
+summary_results = summary_results.astype(data_type_summary_results)
+summary_results = summary_results.drop_duplicates() # just in case, drop duplicates
+print('After changing the data type of each column, the new data info shows:')
+print(summary_results.info())
+# output the summary table
+summary_results.to_csv('PA_York_2024_election_summary.csv', index = False)
 # next steps:
 # because the data contains several election results, try to single out the 2024 presidential election results
 # in line_by_line, each "line" are the results of a specific precinct of a specific election
