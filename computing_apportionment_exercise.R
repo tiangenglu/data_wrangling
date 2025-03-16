751a58371d49b80f7d4b236e0cf3116230814c56
tiangenglu <58899759+tiangenglu@users.noreply.github.com>
2025-03-16 00:32:31 -0400

    2025-03-15

---
+++ computing_apportionment_exercise.R
@@ -0,0 +1,42 @@
+# This R script produces same priority values as published
+# Multiple ways from multiple statistical programming languages can reach to the same results. 
+# This is an informal exercise/test of codes. 
+# Source: https://www2.census.gov/programs-surveys/decennial/2020/data/apportionment/2020PriorityValues.xlsx
+library(readxl)
+library(dplyr)
+table_path = "/Users/tiangeng/Documents/python_files/apportionment-2020-table01.xlsx"
+state_pop <- read_excel(path = table_path,
+                        sheet = 1,
+                        skip = 3,
+                        n_max = 51)
+state_pop <- state_pop |>
+  rename(state = 1, 
+         pop = 2, 
+         apportionment = 3, 
+         change = 4)
+total_pop <- tail(state_pop, n = 1) # last row as total
+state_pop <- state_pop |>
+  filter(row_number() <= n() - 1) # exclude the last row for state population data frame
+# confirm total population match
+sum(state_pop$pop) == total_pop$pop
+# send state-level population to a vector
+pop <- state_pop$pop
+# name the vector
+names(pop) <- state_pop$state
+options(scipen = 999)
+options(digits=20)
+n <- seq(2, 70, by = 1)
+state_priority_value_l <- vector(mode = "list", length = dim(state_pop)[1])
+
+# priority value
+for (i in 1:dim(state_pop)[1]) {
+  state_priority_value_l[[i]]  <- pop[i] / sqrt(n * (n - 1))
+}
+names(state_priority_value_l) <- state_pop$state
+
+state_priority_values <- unlist(state_priority_value_l)
+
+df_state_priority <- data.frame(state = names(state_priority_values),
+                                priority_value = unlist(state_priority_value_l),
+                                row.names = NULL) |>
+  arrange(desc(priority_value))
