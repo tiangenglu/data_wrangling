8302abbf1333f44c7a7403262a4fb80f984a7d12
tiangenglu <58899759+tiangenglu@users.noreply.github.com>
2025-03-21 23:06:24 -0400

    automate summary stats day 2

--- 2025-03-20_EDA_automation.py
+++ 2025-03-20_EDA_automation.py
@@ -4,6 +4,13 @@
 Created on Thu Mar 20 22:03:58 2025
 
 @author: Tiangeng Lu
+
+Expected Output
+
+Data: [data]
+Date: [date]
+
+Subject matter (variable) of interest. Distribution patterns, including count frequency, mean, range, and outliers.
 """
 import os
 import pandas as pd
@@ -12,5 +19,7 @@ from docx.shared import Inches
 my_data = pd.read_csv('iv_alltime.csv')
 my_data.time.unique()
 recent_data = my_data.set_index('time').loc['2025-01-31']
-
-visa_totals=my_data.groupby('visa')['count'].sum()
+# specify the (first) variable of interest
+var_1 = str('visa')
+visa_totals=my_data.groupby(var_1)['count'].sum() # create a series
+group_count=int(visa_totals.describe().iloc[0]) # .iloc[] to avoid warning message
